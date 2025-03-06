c7ed9c1b8f3f159fde44dfd5d84f94cf1916980f
AjemarS <sashabratashchuksasha@gmail.com>
2025-03-06 06:51:17 +0200

    Fixed all current problems with adapter and eslint

---
+++ app/admin/products/add/page.tsx
@@ -0,0 +1,12 @@
+import AddProductForm from "@/components/forms/AddProductForm";
+import React from "react";
+
+export default function ProductsPage() {
+  return (
+    <div className="container mx-auto px-4 py-4">
+      <div className="w-1/3">
+        <AddProductForm />
+      </div>
+    </div>
+  );
+}
